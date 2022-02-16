2cd2391cc1e479264e41c5ee83e42b1970ef0c69
Thuku7 <robethuku11@gmail.com>
2022-02-16 20:36:37 +0300

    Create a user component,import the user class into the componet and also interpolate the date on the component

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit , Input} from '@angular/core';
+import { User } from '../user';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css']
+})
+export class UserProfileComponent implements OnInit {
+
+
+  @Input() userProfile! : any
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
