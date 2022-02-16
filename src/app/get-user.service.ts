60f248a37a843606e03632b484533fae394f7aed
Thuku7 <robethuku11@gmail.com>
2022-02-16 21:05:47 +0300

    Create a service and import the http client to the service,it will also set the Api url in the environment

---
+++ src/app/get-user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetUserService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  private apiUrl = environment.apiUrl
+  private token = environment.token
+  private clientId = environment.clientId
+  private clientSecret = environment.clientSecret
+
+  getUserData(searchQuery:any){
+    return this.httpClient.get(`https://api.github.com/users/${searchQuery}?${this.clientId}&client_secret=${this.clientSecret}`)
+  }
+
+  getUserRepos(searchQuery:any){
+    return this.httpClient.get(`${this.apiUrl}${searchQuery}/repos?${this.token}`)
+  }
+}
