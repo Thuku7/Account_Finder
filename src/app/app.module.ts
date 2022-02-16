d393e5bd454cfe903a0767a9bef1e91c70c99601
Thuku7 <robethuku11@gmail.com>
2022-02-16 20:48:09 +0300

    will create a user repo component to display the repos and import the class repository for its construction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserReposComponent } from './user-repos/user-repos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    UserReposComponent
   ],
   imports: [
     BrowserModule,
