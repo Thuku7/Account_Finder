9c1c4ae0bfe7b81fa07e1cbb575e75ceaa669ac0
Thuku7 <robethuku11@gmail.com>
2022-02-17 04:09:51 +0300

    will change the service private details

--- src/app/get-user.service.ts
+++ src/app/get-user.service.ts
@@ -10,10 +10,10 @@ export class GetUserService {
 
   constructor(private httpClient: HttpClient) { }
 
-  private apiUrl = environment.apiUrl
-  private token = environment.token
-  private clientId = environment.clientId
-  private clientSecret = environment.clientSecret
+  private apiUrl = '';
+  private clientId =  '8864018f0f4565e186eb';
+  private clientSecret = 'c08f9cac2efd2256c29b5cdd882f00285376a747';
+  private token = "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"
 
   getUserData(searchQuery:any){
     return this.httpClient.get(`https://api.github.com/users/${searchQuery}?${this.clientId}&client_secret=${this.clientSecret}`)
